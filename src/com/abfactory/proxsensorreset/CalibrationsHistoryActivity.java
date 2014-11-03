31c36f44335761d6c8f3287d3627f18b133c59c3
Raistlin <Raistlin@192.168.1.33>
2014-11-03 16:59:12 +0100

    History first draft

--- src/com/abfactory/proxsensorreset/CalibrationsHistoryActivity.java
+++ src/com/abfactory/proxsensorreset/CalibrationsHistoryActivity.java
@@ -1,7 +1,12 @@
 package com.abfactory.proxsensorreset;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CalibrationsHistoryActivity extends Activity {
 	
@@ -9,6 +14,20 @@ public class CalibrationsHistoryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calibrations_history_list);
-	}
+		
+		ListView listviewHistory = (ListView) findViewById(R.id.listviewHistory);
+		
+        // Here is where the list should be called first time
+        List<String> history = new ArrayList<String>();
+        history.add("foo");
+        history.add("bar");
 
+        // Use adapter to display history
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, 
+                android.R.layout.simple_list_item_1,
+                history );
+
+        listviewHistory.setAdapter(arrayAdapter); 		
+	}
 }
